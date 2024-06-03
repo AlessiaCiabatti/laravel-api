1c5e97c767e123004fc9556f2c16319f66b4fc94
AlessiaCiabatti <alessiaciaba@gmail.com>
2024-06-03 21:16:26 +0200

    [add] PageController: getTypes and getTechnologies [add] api.php: route: tags and technologies

--- app/Http/Controllers/Api/PageController.php
+++ app/Http/Controllers/Api/PageController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Project;
+use App\Models\Type;
+use App\Models\Technology;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
@@ -11,5 +13,15 @@ class PageController extends Controller
     public function index(){
         $projects = Project::paginate(10);
         return response()->json($projects);
+    }
+
+    public function getTypes(){
+        $types = Type::all();
+        return response()->json($types);
+    }
+
+    public function getTechnologies(){
+        $technologies = Technology::all();
+        return response()->json($technologies);
     }
 }
